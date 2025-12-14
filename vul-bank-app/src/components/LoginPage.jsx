a8eb02d7810b1eda3958f177e01906304f15b2c4
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2025-12-14 11:17:27 +0530

    change password input type to text for demonstration purposes

--- vul-bank-app/src/components/LoginPage.jsx
+++ vul-bank-app/src/components/LoginPage.jsx
@@ -180,7 +180,7 @@ const LoginPage = () => {
                 <input
                   id="password"
                   name="password"
-                  type="password"
+                  type="text"
                   required
                   className="mt-1 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                   placeholder="Try: ' OR 1=1 --"
